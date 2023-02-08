15c0dc29934a8ee073065250ac50a7f13d72150a
luisdasilvahenrique <luisenriklh1177027@gmail.com>
2023-02-08 19:25:23 -0300

    Create quote | Delete Quote | List Quote by Data

--- src/server.ts
+++ src/server.ts
@@ -75,7 +75,59 @@ app.delete('/users/:id', async (req, res) => {
 // Quotes 
 
 // Create Quote
-   
+app.post("/quotes", async (req, res) => {
+    try {
+        const { phrase } = req.body;
+        const quotes = await prisma.quote.create({
+            data: {
+                phrase: phrase,
+                user: {
+                    connect: {
+                        id: 4
+                    },
+                },
+            },
+            include: {
+                user: true
+            }
+        });
+        res.status(201).json(quotes);
+    } catch (error) {
+        res.status(500).json({ error: "Erro na criação da citação!" });
+    }
+});
+
+// Excluir uma citação
+app.delete("/quote/:id", async (req, res) => {
+  try {
+    const quote = await prisma.quote.delete({
+      where: {
+        id: Number(req.params.id),
+      },
+    });
+    res.status(204).end()
+  } catch (error) {
+    res.json({ error: "Error ao deletar citação!" });
+  }
+});
+
+// Visualizar listagem de todas as citações de todos usuários, ordenadas por data
+app.get('/quotes', async (req, res) => {
+    try {
+      const quote = await prisma.quote.findMany({
+        orderBy: {
+          phrase: 'asc'
+        },
+        include: {
+          user: true
+        }
+      });
+      res.status(200).json(quote);
+    } catch (error) {
+      res.status(500).json({ error: 'Error ao listar todas citações!' });
+    }
+  });
+
 
 
 
